03fa5c00d7dcc8aa04ef8bce7ac23c2d466661ed
Paul Guzmán <pguzman@bcoord.cl>
2025-02-12 23:56:04 -0300

    fix vercel

--- app.py
+++ app.py
@@ -23,5 +23,9 @@ app.register_blueprint(folder_routes, url_prefix='/api')
 app.register_blueprint(ubicaciones_routes, url_prefix='/api')
 app.register_blueprint(home_routes)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
